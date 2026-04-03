02363f8053fbb0a46c41640e1db6fd0724765d3b
k-mohan-cts <kommindala.mohan@cognizant.com>
2026-04-03 09:15:59 +0530

    Inital file structure for the Audit and compliance Management

---
+++ src/test/java/org/cognizant/auditcompliancemanagement/AuditComplianceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cognizant.auditcompliancemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditComplianceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
